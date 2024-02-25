3a577c3d02eff5b08b67229c32f36a41d2cb5d61
saniya <sansk099@gmail.com>
2024-02-25 11:25:13 +0530

    modified speed of snake

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -23,7 +23,7 @@ screen.onkey(snake.right,"Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.4)
     snake.move()
 
     #detect collision with food
