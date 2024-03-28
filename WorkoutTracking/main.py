cca959b31083cddc3bf4d592036cce56b2a9759c
saniya <sansk099@gmail.com>
2024-03-28 14:06:29 +0530

    Initial commit

---
+++ WorkoutTracking/main.py
@@ -0,0 +1,43 @@
+from credentials import *
+import requests
+from datetime import datetime
+
+exercise_text = input("Tell me which exercise you dd: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(EXERCISE_ENDPOINT, json=parameters, headers=headers)
+#print(response)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+bearer_headers = {
+    "Authorization": "Bearer BEARER_TOKEN"
+}
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(SHEET_ENDPOINT, json=sheet_inputs, headers=bearer_headers)
+    print(sheet_response.text)
